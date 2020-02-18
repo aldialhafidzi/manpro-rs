9bc129570685bb4ef47daaad5e8949b3ffe38bc2
aldialhafidzi <aldi.alhafidzi@gmail.com>
2020-02-19 06:31:14 +0700

    Initial commit

---
+++ application/database/Seeder.php
@@ -0,0 +1,34 @@
+<?php
+class Seeder
+{
+    private $CI;
+    protected $db;
+    protected $dbforge;
+
+    public function __construct()
+    {
+        $this->CI = &get_instance();
+        $this->CI->load->database();
+        $this->CI->load->dbforge();
+        $this->db = $this->CI->db;
+        $this->dbforge = $this->CI->dbforge;
+    }
+
+    /**
+     * Run another seeder
+     * 
+     * @param string $seeder Seeder classname
+     */
+    public function call($seeder)
+    {
+        $file = APPPATH . 'database/seeds/' . $seeder . '.php';
+        require_once $file;
+        $obj = new $seeder;
+        $obj->run();
+    }
+
+    public function __get($property)
+    {
+        return $this->CI->$property;
+    }
+}
