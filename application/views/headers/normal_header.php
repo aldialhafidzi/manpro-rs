632a5870da7b11c2eae6426a2d92417cd536cab0
Aldi Alhafidzi <aldi.alhafidzi@gmail.com>
2020-02-28 08:15:54 +0700

    fixing header ++

--- application/views/headers/normal_header.php
+++ application/views/headers/normal_header.php
@@ -28,6 +28,11 @@ to get the desired effect
     <div class="wrapper">
 
         <nav class="main-header navbar navbar-expand navbar-white navbar-light">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link" data-widget="pushmenu" href="#"><i class="fas fa-bars"></i></a>
+                </li>
+            </ul>
             <form class="form-inline ml-3">
                 <div class="input-group input-group-sm">
                     <input class="form-control form-control-navbar" type="search" placeholder="Search" aria-label="Search">
