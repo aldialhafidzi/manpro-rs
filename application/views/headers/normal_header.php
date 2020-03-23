f4cd6438e858831d78b6f0731ffb98e81f254dfb
Aldi Alhafidzi <aldi.alhafidzi@gmail.com>
2020-03-23 14:38:48 +0700

    fix style

--- application/views/headers/normal_header.php
+++ application/views/headers/normal_header.php
@@ -12,7 +12,7 @@
     <link rel="stylesheet" href="<?= base_url() ?>public/plugins/sweetalert2-theme-bootstrap-4/bootstrap-4.min.css">
     <link rel="stylesheet" href="<?= base_url() ?>public/plugins/toastr/toastr.min.css">
     <link rel="stylesheet" href="<?= base_url() ?>public/dist/css/adminlte.css">
-    <link rel="stylesheet" href="<?= base_url() ?>public/dist/css/style-gue.css">
+    <link rel="stylesheet" href="<?= base_url() ?>public/dist/scss/style-gue.css">
     <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
     <link rel="stylesheet" href="<?= base_url() ?>public/plugins/select2/css/select2.min.css">
     <link rel="stylesheet" href="<?= base_url() ?>public/plugins/select2-bootstrap4-theme/select2-bootstrap4.min.css">
